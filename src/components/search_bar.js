e6c947493e8f73d3e3cdec2d85686680b4db2f43
Chris Violette <cvioletterun@gmail.com>
2018-01-15 17:06:58 -0500

    Cleaning up code with some ES6 syntax

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Create a class component extending React.Component
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   // Every class component needs a render() method
   render() {
     // Every render() method must return something
