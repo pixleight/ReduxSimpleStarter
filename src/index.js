094273f1e23d1ab80c531f0e817f6416c50d3a3d
Chris Violette <cvioletterun@gmail.com>
2018-01-17 21:04:15 -0500

    Setting up the basic video list component & importing it into main app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyA5tXYUFCSbNd941qOhImLkdlfqLEM--EU';
 
@@ -29,6 +30,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList />
       </div>
     )
   }
