e5182341cd544f4dd680a172287aad9bcb957834
Chris Violette <cvioletterun@gmail.com>
2018-01-17 20:52:05 -0500

    Refactoring component to turn it into a class component & set some state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// Import React and React DOM
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -16,12 +15,20 @@ YTSearch({
 })
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
