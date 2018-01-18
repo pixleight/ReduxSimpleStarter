19e6a0b919463ff12071b2dba3b49a07a99f8931
Chris Violette <cvioletterun@gmail.com>
2018-01-17 20:21:05 -0500

    Starting to talk about component state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 // Create a class component extending React.Component
 class SearchBar extends Component {
+  // constructor() is automatically called whenever a new instance of component is created
+  // Used for setup
+  constructor(props) {
+    // always call super()
+    super(props);
+
+    // initialize state, create new object and assign to this.state
+    this.state = { term: '' }
+  }
+
   // Every class component needs a render() method
   render() {
     // Every render() method must return something
