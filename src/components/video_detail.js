73203d6d62bf26e08af9facd6ed5e5d64ed69914
Chris Violette <cvioletterun@gmail.com>
2018-01-17 21:38:44 -0500

    Set up boilerplate functional component for VideoDetail

---
+++ src/components/video_detail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoDetail = (props) => {
+
+};
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyA5tXYUFCSbNd941qOhImLkdlfqLEM--EU';
 
