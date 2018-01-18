79b510872dc328e64d164b2fb3e5a4b9b2b0b5a6
Chris Violette <cvioletterun@gmail.com>
2018-01-17 21:52:24 -0500

    Passing firt video to the VideoDetail component

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     )
