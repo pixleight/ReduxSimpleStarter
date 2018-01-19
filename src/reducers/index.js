07d5bc5d9f4f885017bc23ecc4586863138d9e02
Chris Violette <cvioletterun@gmail.com>
2018-01-18 23:04:22 -0500

    Creating a first reducer; function to return a piece of app state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
+// Telling Redux how to create the application state
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// A reducer is just a function that returns a piece of state
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Lord of the Rings' },
+  ]
+}
