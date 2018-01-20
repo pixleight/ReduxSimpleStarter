528d686dd3af8e22255a0cba0d30976a7ca18a78
Chris Violette <cvioletterun@gmail.com>
2018-01-20 12:43:40 -0500

    Binding an action creator to a container

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+// An action is a function
+export function selectBook(book) {
+  console.log('A book has been selected:', book.title);
+}
--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// import action
+import { selectBook } from '../actions/index';
+// bind action to make it flow through reducers
+import { bindActionCreators } from 'redux';
+
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
@@ -28,5 +33,11 @@ function mapStateToProps(state) {
   }
 }
 
-// Connect the state mapping and component, and export as container
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props on the BookList container:
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed to all our reducers:
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a component to a container -- it needs to know about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
