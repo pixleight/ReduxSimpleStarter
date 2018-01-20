aa93516d771c402ff7168462c59ece1569023dfb
Chris Violette <cvioletterun@gmail.com>
2018-01-20 13:24:18 -0500

    Look how easy it is to add minor details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 // A reducer is just a function that returns a piece of state
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Lord of the Rings' },
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 300 },
+    { title: 'The Dark Tower', pages: 500 },
+    { title: 'Lord of the Rings', pages: 1500 },
   ]
 }
